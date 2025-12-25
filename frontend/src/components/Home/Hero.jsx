293ae165a32e9d3705391ddf8dc6d496aed6dee0
Aadarsh <aadarshshrivastava66@gmail.com>
2025-12-25 20:37:41 +0530

    Setup

---
+++ frontend/src/components/Home/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Hero() {
+    return ( 
+        <div className='container'>
+            <div className='row'>
+                <div className='col-6'>
+                    
+                </div>
+                <div className='col-6'>
+
+                </div>
+            </div>
+        </div>
+     );
+}
+
+export default Hero;
